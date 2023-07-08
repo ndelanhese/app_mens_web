f6e5b2adce9becfe71503822338f2cc8b0e2b2eb
ndelanhese <nnathanh3@gmail.com>
2023-07-07 22:47:12 -0300

    feat(storybook): added on project

---
+++ src/components/buttons/button.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button } from './button'
+
+const meta: Meta = {
+  title: 'Mens Lib/ui/buttons',
+  component: Button,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  args: {
+    children: 'Button',
+  },
+}
