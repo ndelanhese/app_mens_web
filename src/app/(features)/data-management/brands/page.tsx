804efc3b4f1db6d46a673f60f065488521044302
ndelanhese <nnathanh3@gmail.com>
2023-08-04 21:40:14 -0300

    feat(brands): added table to list all data

--- src/app/(features)/data-management/brands/page.tsx
+++ src/app/(features)/data-management/brands/page.tsx
@@ -1,3 +1,36 @@
-const Brands = () => <h1>Brands</h1>
+import { cookies } from 'next/headers'
+
+import { api } from '@axios'
+
+import { Brands } from './page.types'
+import { BrandsTable } from './shards/table'
+
+const iterateResponse = (brands?: Brands) => {
+  if (!brands) return []
+  return brands?.data?.map((user) => ({
+    name: user?.name,
+  }))
+}
+
+const getBrands = async () => {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')?.value
+    const { data } = await api.get<Brands>('/brands', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return data
+  } catch (error: Error | any) {
+    console.log(error?.response?.data?.message)
+  }
+}
+
+const Brands = async () => {
+  const brands = await getBrands()
+  const rows = iterateResponse(brands)
+  return <BrandsTable rows={rows} />
+}
 
 export default Brands
---
+++ src/app/(features)/data-management/brands/shards/table.types.ts
@@ -0,0 +1,7 @@
+export type Brands = {
+  name: string
+}
+
+export type BrandsTableProps = {
+  rows: Array<Brands>
+}
