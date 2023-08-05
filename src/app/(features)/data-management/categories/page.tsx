43cfae2d2a495a0589db072084a4f7a0f05d2b3a
ndelanhese <nnathanh3@gmail.com>
2023-08-04 21:57:23 -0300

    feat(categories): added categories table list

--- src/app/(features)/data-management/categories/page.tsx
+++ src/app/(features)/data-management/categories/page.tsx
@@ -1,3 +1,36 @@
-const Categories = () => <h1>Categories</h1>
+import { cookies } from 'next/headers'
+
+import { api } from '@axios'
+
+import { Categories } from './page.types'
+import { CategoriesTable } from './shards/table'
+
+const iterateResponse = (categories?: Categories) => {
+  if (!categories) return []
+  return categories?.data?.map((user) => ({
+    name: user?.name,
+  }))
+}
+
+const getCategories = async () => {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')?.value
+    const { data } = await api.get<Categories>('/categories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return data
+  } catch (error: Error | any) {
+    console.log(error?.response?.data?.message)
+  }
+}
+
+const Categories = async () => {
+  const categories = await getCategories()
+  const rows = iterateResponse(categories)
+  return <CategoriesTable rows={rows} />
+}
 
 export default Categories
