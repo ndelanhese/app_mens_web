2db7eefa3f4d1b4676b7a0a4a93428f0125711f2
ndelanhese <nnathanh3@gmail.com>
2023-08-04 22:19:59 -0300

    feat(products): added table

--- src/app/(features)/data-management/products/page.tsx
+++ src/app/(features)/data-management/products/page.tsx
@@ -1,3 +1,44 @@
-const Products = () => <h1>Products</h1>
+import { cookies } from 'next/headers'
+
+import { api } from '@axios'
+
+import { Products } from './page.types'
+import { ProductsTable } from './shards/table'
+
+const iterateResponse = (products?: Products) => {
+  if (!products) return []
+  return products?.data?.map((product) => ({
+    name: product?.name,
+    partNumber: product?.part_number,
+    description: product?.description,
+    price: product?.price,
+    size: product?.size,
+    color: product?.color,
+    quantity: product?.quantity,
+    category: product?.category?.name,
+    brand: product?.brand?.name,
+  }))
+}
+
+const getProducts = async () => {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')?.value
+    const { data } = await api.get<Products>('/products', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return data
+  } catch (error: Error | any) {
+    console.log(error?.response?.data?.message)
+  }
+}
+
+const Products = async () => {
+  const categories = await getProducts()
+  const rows = iterateResponse(categories)
+  return <ProductsTable rows={rows} />
+}
 
 export default Products
---
+++ src/app/(features)/data-management/products/shards/table.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { useMemo } from 'react'
+
+import { Table } from '@/components/shared/table/table'
+import { TableColumn } from '@/components/shared/table/table.types'
+
+import { TableColumnHeader } from '@components/shared/table/tableColumnHeader'
+
+import { Product, ProductsTableProps } from './table.types'
+
+export const ProductsTable = ({ rows }: ProductsTableProps) => {
+  const tableColumns: Array<TableColumn<Product>> = useMemo(
+    () => [
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Nome" />
+        ),
+        accessorKey: 'name',
+        id: 'Nome',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Código" />
+        ),
+        accessorKey: 'partNumber',
+        id: 'Código',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Descrição" />
+        ),
+        accessorKey: 'description',
+        id: 'Descrição',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Preço" />
+        ),
+        accessorKey: 'price',
+        id: 'Preço',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Tamanho" />
+        ),
+        accessorKey: 'size',
+        id: 'Tamanho',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Cor" />
+        ),
+        accessorKey: 'color',
+        id: 'Cor',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Quantidade" />
+        ),
+        accessorKey: 'quantity',
+        id: 'Quantidade',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Categoria" />
+        ),
+        accessorKey: 'category',
+        id: 'Categoria',
+      },
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Marca" />
+        ),
+        accessorKey: 'brand',
+        id: 'Marca',
+      },
+    ],
+    [],
+  )
+
+  const handleRowClick = (row: Product) => {
+    console.log(row)
+  }
+
+  return (
+    <Table
+      tableColumns={tableColumns}
+      filter="Nome"
+      rows={rows}
+      actionLabel="Ação"
+      actionCallback={handleRowClick}
+    />
+  )
+}
