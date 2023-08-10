7148d2eb977822556da572fc9ef2f21aecb62fed
ndelanhese <nnathanh3@gmail.com>
2023-08-10 19:42:01 -0300

    feat(promotions): added promotions list

--- src/app/(features)/promotions/promotions/page.tsx
+++ src/app/(features)/promotions/promotions/page.tsx
@@ -1,9 +1,51 @@
+import { cookies } from 'next/headers'
+import { cache } from 'react'
 import { Metadata } from 'next'
+
+import { api } from '@axios'
+
+import { Promotions } from './page.types'
+import { PromotionsTable } from './shards/table'
+
+const iterateResponse = (promotions?: Promotions) => {
+  if (!promotions) return []
+  return promotions?.data?.map((promotion) => ({
+    name: promotion?.name,
+    description: promotion?.description,
+    discount: String(promotion?.discount_amount), // TODO -> add util to format currency
+    initialDate: promotion?.initial_date, // TODO -> add util to format date
+    finalDate: promotion?.final_date, // TODO -> add util to format date
+    status: promotion?.status,
+    category: promotion?.category?.name,
+    products: promotion?.products
+      ?.map(({ product }) => product?.name)
+      .join(', '), // TODO -> add util to format array and prevent duplicate
+  }))
+}
+
+const getPromotions = cache(async () => {
+  try {
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('token')?.value
+    const { data } = await api.get<Promotions>('/promotions', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return data
+  } catch (error: Error | any) {
+    console.log(error?.response?.data?.message)
+  }
+})
 
 export const metadata: Metadata = {
   title: 'Promoções',
 }
 
-const Promotions = () => <h1>Promotions</h1>
+const Promotions = async () => {
+  const promotions = await getPromotions()
+  const rows = iterateResponse(promotions)
+  return <PromotionsTable rows={rows} />
+}
 
 export default Promotions
