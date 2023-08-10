c24c4435d51036425dbf8386b6a60f0c3bdb2972
ndelanhese <nnathanh3@gmail.com>
2023-08-10 19:15:05 -0300

    feat(promotion-categories): created list

---
+++ src/app/(features)/promotions/promotion-categories/shards/table.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useMemo } from 'react'
+
+import { Table } from '@/components/shared/table/table'
+import { TableColumn } from '@/components/shared/table/table.types'
+
+import { TableColumnHeader } from '@components/shared/table/tableColumnHeader'
+
+import { Category, CategoriesTableProps } from './table.types'
+
+export const CategoriesTable = ({ rows }: CategoriesTableProps) => {
+  const tableColumns: Array<TableColumn<Category>> = useMemo(
+    () => [
+      {
+        header: ({ column }) => (
+          <TableColumnHeader column={column} title="Nome" />
+        ),
+        accessorKey: 'name',
+        id: 'Nome',
+      },
+    ],
+    [],
+  )
+
+  const handleRowClick = (row: Category) => {
+    console.log(row)
+  }
+
+  return (
+    <Table
+      tableColumns={tableColumns}
+      filter="Nome"
+      rows={rows}
+      actionLabel="Ação"
+      actionCallback={handleRowClick}
+    />
+  )
+}
