69c519436911f8bcaaaab6d2bd610ec126931239
ndelanhese <nnathanh3@gmail.com>
2023-09-18 21:36:06 -0300

    feat(employees): added all attributes on employees create

--- src/components/features/employees/employees/client/createEmployeeForm/createEmployeeForm.schema.ts
+++ src/components/features/employees/employees/client/createEmployeeForm/createEmployeeForm.schema.ts
@@ -3,7 +3,20 @@ import { z } from 'zod';
 export const employeeFormSchema = z.object({
   name: z.string().nonempty('O nome é obrigatário'),
   cpf: z.string().nonempty('O CPF é obrigatário'),
+  rg: z.string().nullable(),
+  birth_date: z.string().nonempty('A data de nascimento é obrigatária'),
   phone: z.string().nonempty('O telefone é obrigatário'),
+  pis_pasep: z.string().nonempty('O PIS/PASEP é obrigatário'),
+  admission_date: z.string().nonempty('A data de admissão é obrigatária'),
+  resignation_date: z.string().nullable(),
+  address: z.object({
+    address: z.string().nonempty('O endereço é obrigatário'),
+    number: z.string().nonempty('O número é obrigatório'),
+    district: z.string().nonempty('O bairro é obrigatório'),
+    postal_code: z.string().nonempty('O CEP é obrigatório'),
+    city: z.string().nonempty('A cidade é obrigatória'),
+    state: z.string().nonempty('O estado é obrigatório'),
+  }),
 });
 
 export type EmployeeFormSchema = z.infer<typeof employeeFormSchema>;
--- src/components/features/employees/employees/client/createEmployeeForm/createEmployeeForm.tsx
+++ src/components/features/employees/employees/client/createEmployeeForm/createEmployeeForm.tsx
@@ -42,20 +42,19 @@ const CreateEmployeeFormComponent = ({
       );
       handleCloseModal();
       toast({
-        title: 'Produto criado com sucesso',
+        title: 'Funcionário criado com sucesso',
         variant: 'default',
       });
     } catch (error: Error | any) {
       const errorMessage = error.response.data.message ?? 'Erro desconhecido';
       toast({
-        title: 'Erro ao criar o produto',
+        title: 'Erro ao criar o funcionário',
         description: errorMessage,
         variant: 'destructive',
       });
     }
   };
 
-  // TODO -> add all attributes
   return (
     <form
       className="grid w-full grid-cols-1 gap-4 overflow-y-auto sm:h-auto sm:max-h-[30rem] sm:grid-cols-2"
@@ -72,7 +71,18 @@ const CreateEmployeeFormComponent = ({
         label="CPF"
         register={register}
         errorMessage={errors.cpf?.message}
-        disabled
+      />
+      <ControlledInput
+        id="rg"
+        label="RG"
+        register={register}
+        errorMessage={errors.rg?.message}
+      />
+      <ControlledInput
+        id="birth_date"
+        label="Data de nascimento"
+        register={register}
+        errorMessage={errors.birth_date?.message}
       />
       <ControlledInput
         id="phone"
@@ -80,8 +90,61 @@ const CreateEmployeeFormComponent = ({
         register={register}
         errorMessage={errors.phone?.message}
       />
-
-      <Button disabled={isSubmitting} type="submit" className="sm:self-end">
+      <ControlledInput
+        id="pis_pasep"
+        label="PIS/PASEP"
+        register={register}
+        errorMessage={errors.pis_pasep?.message}
+      />
+      <ControlledInput
+        id="admission_date"
+        label="Data de admissão"
+        register={register}
+        errorMessage={errors.admission_date?.message}
+      />
+      <ControlledInput
+        id="resignation_date"
+        label="Data de demissão"
+        register={register}
+        errorMessage={errors.resignation_date?.message}
+      />
+      <ControlledInput
+        id="address.address"
+        label="Endereço"
+        register={register}
+        errorMessage={errors.address?.address?.message}
+      />
+      <ControlledInput
+        id="address.number"
+        label="Número"
+        register={register}
+        errorMessage={errors.address?.number?.message}
+      />
+      <ControlledInput
+        id="address.district"
+        label="Bairro"
+        register={register}
+        errorMessage={errors.address?.district?.message}
+      />
+      <ControlledInput
+        id="address.postal_code"
+        label="CEP"
+        register={register}
+        errorMessage={errors.address?.postal_code?.message}
+      />
+      <ControlledInput
+        id="address.city"
+        label="Cidade"
+        register={register}
+        errorMessage={errors.address?.city?.message}
+      />
+      <ControlledInput
+        id="address.state"
+        label="Estado"
+        register={register}
+        errorMessage={errors.address?.state?.message}
+      />
+      <Button disabled={isSubmitting} type="submit" className="sm:col-start-2">
         Criar
       </Button>
     </form>
