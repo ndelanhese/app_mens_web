3582510d8797991b86db07258c43b4ebfcb42242
ndelanhese <nnathanh3@gmail.com>
2023-10-16 20:34:52 -0300

    feat(sidebar): created sidebar context

---
+++ src/contexts/sidebarDrawer/sidebarDrawer.context.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+
+import {
+  SidebarDrawerContextData,
+  SidebarDrawerProviderProps,
+} from './sidebarDrawer.types';
+import { parseCookies, setCookie } from 'nookies';
+
+export const SidebarDrawerContext = createContext<SidebarDrawerContextData>(
+  {} as SidebarDrawerContextData,
+);
+
+export const SidebarDrawerProvider = ({
+  children,
+}: SidebarDrawerProviderProps) => {
+  const { 'mens.sidebar_status': sidebarStatus } = parseCookies();
+
+  const sidebarIsOpenOnCookies = sidebarStatus !== 'false';
+
+  const [open, setOpen] = useState<boolean>(sidebarIsOpenOnCookies ?? true);
+
+  const handleToggleSidebar = useCallback(() => {
+    setOpen(!open);
+  }, [open]);
+
+  useEffect(() => {
+    const THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
+    setCookie(undefined, 'mens.sidebar_status', String(open), {
+      maxAge: THIRTY_DAYS_IN_SECONDS,
+      path: '/',
+    });
+  }, [open]);
+
+  const memoizedSidebarStatus = useMemo(
+    () => ({ open, handleToggleSidebar }),
+    [open, handleToggleSidebar],
+  );
+  return (
+    <SidebarDrawerContext.Provider value={memoizedSidebarStatus}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+};
