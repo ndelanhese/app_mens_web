aae2d567c244a353ba0dc655fa3f833b4bfccd88
ndelanhese <nnathanh3@gmail.com>
2023-11-01 21:36:31 -0300

    refactor(products): changed products form

--- src/components/shared/formGrid/formGrid.tsx
+++ src/components/shared/formGrid/formGrid.tsx
@@ -4,7 +4,7 @@ import { FormGridProps } from './formGrid.types';
 export const FormGrid = ({ children, className }: FormGridProps) => (
   <form
     className={twMerge(
-      'grid w-full grid-cols-1 gap-4 overflow-y-auto sm:h-auto sm:max-h-[30rem] sm:grid-cols-2',
+      'grid w-full grid-cols-1 gap-4 overflow-y-auto sm:h-auto sm:max-h-[40rem] sm:grid-cols-2',
       className,
     )}
   >
