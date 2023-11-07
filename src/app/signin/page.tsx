9da22f1af82cb460ec4fae3e6bd7a0eac2aabdfb
ndelanhese <nnathanh3@gmail.com>
2023-11-06 21:52:25 -0300

    refactor(login): change login layout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
+import NextImage from 'next/image';
 
-import { User } from 'lucide-react';
 import { SigninForm } from './shards/signinForm';
 
 export const metadata: Metadata = {
@@ -10,9 +10,18 @@ export const metadata: Metadata = {
 const SignIn = () => {
   return (
     <section className="flex h-full w-full flex-col items-center justify-center gap-7">
-      <User className="h-[6rem] w-[6rem] rounded-full border border-black-80 p-4 dark:border-white-40 dark:text-white-40" />
+      <div className="h-[7rem] w-[7rem] overflow-hidden rounded-full border dark:border-white-40">
+        <NextImage
+          src={'/user-icon.png'}
+          alt="avatar-image"
+          height={112}
+          width={112}
+        />
+      </div>
       <div className="flex flex-col items-center justify-center gap-2">
-        <h1 className="dark:text-white text-xl font-sb">Entrar no sistema</h1>
+        <h1 className="dark:text-white text-xl font-sb">
+          Loja Men&apos;s Modas
+        </h1>
         <p className="text-md dark:text-white-40">
           Entre com seu usuário e senha
         </p>
