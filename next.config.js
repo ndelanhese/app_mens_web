7e8402b7584dff955cadf10526182003c2e233ac
ndelanhese <nnathanh3@gmail.com>
2023-12-27 14:33:36 -0300

    feat(dashboard): create dashboard base component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
