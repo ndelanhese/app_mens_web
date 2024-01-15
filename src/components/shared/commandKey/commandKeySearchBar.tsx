2877c981592f41a93a6427cc099a2882af078e65
ndelanhese <nnathanh3@gmail.com>
2024-01-15 20:18:58 -0300

    feat(comand-key): re-style

--- src/components/shared/commandKey/commandKeySearchBar.tsx
+++ src/components/shared/commandKey/commandKeySearchBar.tsx
@@ -2,27 +2,19 @@
 
 import { useState } from 'react';
 
-import { Input } from '@components/ui/shadcn/input';
+import { Button } from '@components/ui/shadcn/button';
 
 import { CommandKey } from './commandKey';
-import { Search } from 'lucide-react';
 
 export const CommandKeySearchBar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative hidden items-center sm:flex">
-      <div className="absolute left-2 flex flex-row items-center gap-1">
-        <Search className="h-4 w-4" />
-      </div>
-      <Input
-        className="bg-white border border-black-10 pl-8 pr-11 dark:border-white-10 dark:bg-zinc-950"
-        placeholder="Pesquisar..."
-        onClick={() => setOpen(open => !open)}
-      />
-      <div className="absolute right-2 flex flex-row items-center gap-1">
-        <CommandKey open={open} setOpen={setOpen} />
-      </div>
+    <div className="hidden cursor-pointer items-center space-x-3 rounded-md border border-white-5 px-2 py-2 sm:flex">
+      <span onClick={() => setOpen(open => !open)} className="text-sm">
+        Pesquise um serviço
+      </span>
+      <CommandKey open={open} setOpen={setOpen} />
     </div>
   );
 };
