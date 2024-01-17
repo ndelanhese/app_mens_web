41e80747402f7105c64a79e27322fc631f7cf2c8
ndelanhese <nnathanh3@gmail.com>
2024-01-17 19:16:21 -0300

    feat(dashboard): add low stock products modal

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,10 @@
-const path = require('path');
+const path = require("path");
 
-const handleBuildEslintCommand = filenames =>
-  `next lint --fix --file ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+const handleBuildEslintCommand = (filenames) =>
+	`biome check --apply ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(" ")}`;
 
 module.exports = {
-  '*.{js,ts,tsx}': [
-    'prettier --write',
-    'eslint --fix',
-    handleBuildEslintCommand,
-  ],
+	"*.{js,ts,tsx}": ["biome check --apply", handleBuildEslintCommand],
 };
--- src/components/features/dashboard/dashboard.tsx
+++ src/components/features/dashboard/dashboard.tsx
@@ -1,97 +1,109 @@
-'use client';
+"use client";
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import { Button } from '@components/ui/shadcn/button';
+import { RefModalProps } from "@components/shared/table/table.types";
+import { TableDialog } from "@components/shared/table/tableDialog";
+import { Button } from "@components/ui/shadcn/button";
 import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@components/ui/shadcn/tabs';
-import { TableDialog } from '@components/shared/table/tableDialog';
-import { RefModalProps } from '@components/shared/table/table.types';
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "@components/ui/shadcn/tabs";
 
-import { CalendarDateRangePicker } from './components/dateRangePicker';
-import { OverviewTab } from './components/tabs/overViewTab';
-import { Download } from 'lucide-react';
-import { AnalyticsTab } from './components/tabs/analyticsTab';
-import { ReportsTab } from './components/tabs/reportsTab';
-import { NotificationsTab } from './components/tabs/notificationsTab';
-import { parseCookies } from 'nookies';
-import { ProductsStockResponse } from './dashboard.types';
+import { Download } from "lucide-react";
+import { destroyCookie, parseCookies } from "nookies";
+import { CalendarDateRangePicker } from "./components/dateRangePicker";
+import { ProductsWithLowStockTable } from "./components/producsWithLowStockTable";
+import { AnalyticsTab } from "./components/tabs/analyticsTab";
+import { NotificationsTab } from "./components/tabs/notificationsTab";
+import { OverviewTab } from "./components/tabs/overViewTab";
+import { ReportsTab } from "./components/tabs/reportsTab";
+import { ProductsStockResponse } from "./dashboard.types";
 
 export const Dashboard = () => {
-  const productsStockModalRef = useRef<RefModalProps | null>(null);
+	const productsStockModalRef = useRef<RefModalProps | null>(null);
 
-  const [value, setValue] = useState<string>('overview');
+	const [value, setValue] = useState<string>("overview");
+	const [productsWithLowStock, setProductsWithLowStock] = useState<
+		ProductsStockResponse | undefined
+	>(undefined);
 
-  useEffect(() => {
-    const { 'stock-notifications-mens-modas': stockNotifications } =
-      parseCookies();
-    const productsWithLowQty: ProductsStockResponse =
-      JSON.parse(stockNotifications);
+	useEffect(() => {
+		const { "stock-notifications-mens-modas": stockNotifications } =
+			parseCookies();
+		const productsWithLowQty: ProductsStockResponse | undefined =
+			stockNotifications ? JSON?.parse(stockNotifications) : undefined;
 
-    if (productsWithLowQty && productsWithLowQty.data.length > 0) {
-      productsStockModalRef.current?.open();
-      console.log(productsWithLowQty);
-    }
-  }, []);
+		if (productsWithLowQty && productsWithLowQty.data.length > 0) {
+			setProductsWithLowStock(productsWithLowQty);
+			destroyCookie(null, "stock-notifications-mens-modas");
+		}
+	}, []);
 
-  return (
-    <div>
-      <div className="flex flex-col">
-        <div className="flex-1 space-y-4">
-          <div className="flex flex-col items-start justify-between space-y-2 sm:flex-row sm:items-center">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex w-full items-center justify-between sm:w-auto sm:space-x-2">
-              <CalendarDateRangePicker />
-              {(value === 'analytics' || value === 'reports') && (
-                <Button className="inline-flex gap-2">
-                  <Download className="h-4 w-4" />
-                  <p className="hidden sm:block">Baixar</p>
-                </Button>
-              )}
-            </div>
-          </div>
-          <Tabs
-            defaultValue="overview"
-            value={value}
-            onValueChange={setValue}
-            className="space-y-4"
-          >
-            <TabsList className="w-full sm:w-auto">
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="analytics">Analytics</TabsTrigger>
-              <TabsTrigger value="reports">Reports</TabsTrigger>
-              <TabsTrigger value="notifications">Notifications</TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview">
-              <OverviewTab />
-            </TabsContent>
-            <TabsContent value="analytics">
-              <AnalyticsTab />
-            </TabsContent>
-            <TabsContent value="reports">
-              <ReportsTab />
-            </TabsContent>
-            <TabsContent value="notifications">
-              <NotificationsTab />
-            </TabsContent>
-          </Tabs>
-        </div>
-      </div>
-      {
-        <TableDialog
-          dialogRef={ref => {
-            productsStockModalRef.current = ref;
-          }}
-          content={<h1>teste</h1>}
-          description="Produtos com estoque menor ou igual a 3 unidades."
-          title="Produtos com estoque baixo."
-          isTriggered={false}
-        />
-      }
-    </div>
-  );
+	useEffect(() => {
+		if (productsWithLowStock) {
+			productsStockModalRef.current?.open();
+		}
+	}, [productsWithLowStock]);
+
+	return (
+		<div>
+			<div className="flex flex-col">
+				<div className="flex-1 space-y-4">
+					<div className="flex flex-col items-start justify-between space-y-2 sm:flex-row sm:items-center">
+						<h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+						<div className="flex w-full items-center justify-between sm:w-auto sm:space-x-2">
+							<CalendarDateRangePicker />
+							{(value === "analytics" || value === "reports") && (
+								<Button className="inline-flex gap-2">
+									<Download className="h-4 w-4" />
+									<p className="hidden sm:block">Baixar</p>
+								</Button>
+							)}
+						</div>
+					</div>
+					<Tabs
+						defaultValue="overview"
+						value={value}
+						onValueChange={setValue}
+						className="space-y-4"
+					>
+						<TabsList className="w-full sm:w-auto">
+							<TabsTrigger value="overview">Overview</TabsTrigger>
+							<TabsTrigger value="analytics">Analytics</TabsTrigger>
+							<TabsTrigger value="reports">Reports</TabsTrigger>
+							<TabsTrigger value="notifications">Notifications</TabsTrigger>
+						</TabsList>
+						<TabsContent value="overview">
+							<OverviewTab />
+						</TabsContent>
+						<TabsContent value="analytics">
+							<AnalyticsTab />
+						</TabsContent>
+						<TabsContent value="reports">
+							<ReportsTab />
+						</TabsContent>
+						<TabsContent value="notifications">
+							<NotificationsTab />
+						</TabsContent>
+					</Tabs>
+				</div>
+			</div>
+			{productsWithLowStock && (
+				<TableDialog
+					dialogRef={(ref) => {
+						productsStockModalRef.current = ref;
+					}}
+					content={
+						<ProductsWithLowStockTable products={productsWithLowStock} />
+					}
+					description="Produtos com estoque menor ou igual a 3 unidades."
+					title="Produtos com estoque baixo."
+					isTriggered={false}
+				/>
+			)}
+		</div>
+	);
 };
