158fe3b1121134df1229ada773f46da676f03eb8
ndelanhese <nnathanh3@gmail.com>
2024-01-19 15:52:34 -0300

    feat(dashboard): add reports

--- src/components/features/dashboard/components/tabs/reportsTab.tsx
+++ src/components/features/dashboard/components/tabs/reportsTab.tsx
@@ -1,5 +1,40 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@components/ui/shadcn/tabs';
+
+import { CalendarDateRangePicker } from '../dateRangePicker';
+
 export const ReportsTab = () => (
-  <div className="space-y-4">
-    <h1>Reports</h1>
+  <div>
+    <div className="flex w-full flex-1">
+      <Tabs defaultValue="products" className="w-full space-y-4">
+        <div className="flex w-full flex-col items-start justify-start gap-2 sm:flex-row sm:items-center sm:justify-between">
+          <TabsList>
+            <TabsTrigger value="products">Produtos</TabsTrigger>
+            <TabsTrigger value="methods_of_payments">Mét. de pag.</TabsTrigger>
+            <TabsTrigger value="categories">Categorias</TabsTrigger>
+            <TabsTrigger value="brands">Marcas</TabsTrigger>
+          </TabsList>
+          <div className="flex items-center gap-2">
+            <CalendarDateRangePicker />
+          </div>
+        </div>
+        <TabsContent value="products">
+          <h1>Products tab</h1>
+        </TabsContent>
+        <TabsContent value="methods_of_payments">
+          <h1>Methods of Payments tab</h1>
+        </TabsContent>
+        <TabsContent value="categories">
+          <h1>Categories tab</h1>
+        </TabsContent>
+        <TabsContent value="brands">
+          <h1>Brands tab</h1>
+        </TabsContent>
+      </Tabs>
+    </div>
   </div>
 );
--- src/components/features/dashboard/dashboard.tsx
+++ src/components/features/dashboard/dashboard.tsx
@@ -16,7 +16,6 @@ import { twJoin } from 'tailwind-merge';
 
 import { CalendarDateRangePicker } from './components/dateRangePicker';
 import { ProductsWithLowStockTable } from './components/producsWithLowStockTable';
-import { NotificationsTab } from './components/tabs/notificationsTab';
 import { OverviewTab } from './components/tabs/overViewTab';
 import { ReportsTab } from './components/tabs/reportsTab';
 import { ProductsStockResponse } from './dashboard.types';
@@ -24,7 +23,6 @@ import { ProductsStockResponse } from './dashboard.types';
 export const Dashboard = () => {
   const productsStockModalRef = useRef<RefModalProps | null>(null);
 
-  const [value, setValue] = useState<string>('overview');
   const [productsWithLowStock, setProductsWithLowStock] = useState<
     ProductsStockResponse | undefined
   >(undefined);
@@ -53,42 +51,18 @@ export const Dashboard = () => {
         <div className="flex-1 space-y-4">
           <div className="flex flex-col items-start justify-between space-y-2 sm:flex-row sm:items-center">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div
-              className={twJoin(
-                'flex w-full items-center justify-between sm:w-auto sm:space-x-2',
-                value !== 'reports' && 'sm:py-5',
-              )}
-            >
-              {value === 'reports' && (
-                <>
-                  <CalendarDateRangePicker />
-                  <Button className="inline-flex gap-2">
-                    <Download className="h-4 w-4" />
-                    <p className="hidden sm:block">Baixar</p>
-                  </Button>
-                </>
-              )}
-            </div>
+            <div className="flex w-full items-center justify-between sm:w-auto sm:space-x-2"></div>
           </div>
-          <Tabs
-            defaultValue="overview"
-            value={value}
-            onValueChange={setValue}
-            className="space-y-4"
-          >
+          <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
               <TabsTrigger value="overview">Overview</TabsTrigger>
               <TabsTrigger value="reports">Relatórios</TabsTrigger>
-              <TabsTrigger value="notifications">Notificações</TabsTrigger>
             </TabsList>
             <TabsContent value="overview">
               <OverviewTab />
             </TabsContent>
             <TabsContent value="reports">
               <ReportsTab />
-            </TabsContent>
-            <TabsContent value="notifications">
-              <NotificationsTab />
             </TabsContent>
           </Tabs>
         </div>
